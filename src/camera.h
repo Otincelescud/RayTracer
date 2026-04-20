2eea9e64cd8b659b4e524a4142473473861d7241
Otincelescu Darius Marian <otincelescud@gmail.com>
2026-04-20 21:25:41 +0300

    Write camera code for antialiasing

--- src/camera.h
+++ src/camera.h
@@ -12,7 +12,7 @@ public:
     // Image
     double aspect_ratio = 1.0;
     int image_width = 100;
-    int sample_per_pixel = 10;
+    int samples_per_pixel = 10;
 
     void render(const hittable& world) {
         std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -20,13 +20,12 @@ public:
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++) {
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-
-                write_color(std::cout, pixel_color);
+                color pixel_color(0,0,0);
+                for (int sample = 0; sample < samples_per_pixel; sample++) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, world);
+                }
+                write_color(std::cout, pixel_color * pixel_sample_scale);
             }
         }
 
@@ -36,6 +35,8 @@ public:
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_sample_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -62,6 +63,7 @@ public:
 
 private:
     int image_height; // Rendered image height
+    double pixel_sample_scale;
     point3 center; // Camera center
     point3 pixel00_loc; // Location of pixel 0, 0
     vec3 pixel_delta_u; // Offset of pixel to the right
