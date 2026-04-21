4028d0091e5f70b14ff661870d717a21b2b6817e
Otincelescu Darius Marian <otincelescud@gmail.com>
2026-04-21 22:18:40 +0300

    Update main.cpp to display equilateral triangle

--- src/main.cpp
+++ src/main.cpp
@@ -8,16 +8,25 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "plane.h"
+#include "triangle.h"
 #include "camera.h"
+
+#include <cmath>
 
 int main() {
     // World
 
     hittable_list world;
 
-    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    //world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
     //world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
-    world.add(make_shared<plane>(point3(0, -100.5, -1), vec3(0, 1, 0)));
+    //world.add(make_shared<tri>(point3(0, -1.5, -1), point3(0, -1.5, -101.5), point3(-101.5, -1.5, -1)));
+    vec3 offset(0.0, -0.2, 0.0);
+    world.add(make_shared<tri>(
+        point3(std::sqrt(3.0)/2, -1.0/2, -1.0)+offset,
+        point3(-std::sqrt(3.0)/2, -1.0/2, -1.0)+offset,
+        point3(0.0, 1.0, -1.0)+offset
+    ));
 
     // Use Camera
     camera cam;
